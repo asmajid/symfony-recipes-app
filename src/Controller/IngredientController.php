82dd78ea579091c7a60ac7ef368fc71d9f55284d
Asmaa Majid <majid.asmaa2001@gmail.com>
2024-06-09 23:27:29 +0100

    Add form and associated logic to create a new ingredient

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -2,19 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\Ingredient;
+use App\Form\IngredientType;
 use App\Repository\IngredientRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
 class IngredientController extends AbstractController
 {
     private $repository;
-    public function __construct(IngredientRepository $ingredientRepository)
+    private $em;
+    private $errors;
+    public function __construct(IngredientRepository $ingredientRepository, EntityManagerInterface $entityManager)
     {
         $this->repository = $ingredientRepository;
+        $this->em = $entityManager;
     }
 
     /**
@@ -28,13 +35,37 @@ class IngredientController extends AbstractController
     public function index(PaginatorInterface $paginator, Request $request): Response
     {
         $ingredients = $paginator->paginate(
-            $this->repository->findAll(),
+            $this->repository->findBy([], ['id' => 'DESC']),
             $request->query->getInt('page', 1),
             6
         );
 
         return $this->render('pages/ingredient/index.html.twig', [
             'ingredients' => $ingredients
+        ]);
+    }
+
+    #[Route('/ingredient/nouveau', name: 'app_new', methods: ['GET', 'POST'])]
+    public function new(Request $request): Response
+    {
+        $ingredient = new Ingredient();
+        $form = $this->createForm(IngredientType::class, $ingredient);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ingredient = $form->getData();
+
+            $this->em->persist($ingredient);
+            $this->em->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre ingrédient ' . $ingredient->getName() . 'a été créé avec succès !'
+            );
+            return $this->redirectToRoute('app_ingredient');
+        }
+
+        return $this->render('pages/ingredient/new.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/IngredientType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ingredient;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\LessThan;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Positive;
+use Webmozart\Assert\Assert as AssertAssert;
+
+class IngredientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Entrez le nom de l\'ingrédient...',
+                    'minlength' => 5,
+                    'maxlength' => 50
+                ],
+                'label' => 'Nom',
+                'constraints' => [
+                    new Length(['min' => 2, 'max' => 50]),
+                    new NotBlank()
+                ]
+            ])
+            ->add('price', MoneyType::class, [
+                'attr' => ['placeholder' => 'Prix de l\'ingrédient...'],
+                'label' => 'Prix',
+                'constraints' => [
+                    new Positive(),
+                    new LessThan(200)
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Créer mon ingrédient'
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Ingredient::class,
+        ]);
+    }
+}

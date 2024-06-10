b368095dafa73d7740d8012a5f672fb27e394f7a
Asmaa Majid <majid.asmaa2001@gmail.com>
2024-06-11 00:26:30 +0100

    Add fixtures for Ingredient and Recipe entities with ManyToMany

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ingredient;
+use App\Entity\Recipe;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -22,12 +23,29 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        for ($i = 0; $i < 50; $i++) {
+        $ingredients = [];
+        for ($i = 0; $i < 20; $i++) {
             $ingredient = new Ingredient();
             $ingredient->setName($this->faker->word())
                 ->setPrice(mt_rand(2, 200));
-
+            $ingredients[] = $ingredient;
             $manager->persist($ingredient);
+        }
+
+        for ($j = 0; $j < 10; $j++) {
+            $recipe = new Recipe();
+            $recipe->setName($this->faker->word())
+                ->setTime(mt_rand(0, 1) == 1 ? mt_rand(10, 180) : null)
+                ->setNbPeople(mt_rand(0, 1) == 1 ? mt_rand(1, 50) : null)
+                ->setDifficulty(mt_rand(0, 1) == 1 ? mt_rand(1, 5) : null)
+                ->setDescription($this->faker->text(200))
+                ->setPrice(mt_rand(0, 1) == 1 ? mt_rand(1, 1000) : null)
+                ->setFavorite(mt_rand(0, 1) == 1 ? true : false)
+
+                ->addIngredient($ingredients[mt_rand(0, count($ingredients) - 1)]);
+
+
+            $manager->persist($recipe);
         }
 
         $manager->flush();
